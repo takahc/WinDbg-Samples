6f717cfabe6db71f1f2a8047589b5ea9c17fcd01
takahc <108157287+takahc@users.noreply.github.com>
2025-10-05 11:57:53 +0900

    Add MyApp project files and configurations for TTD DAP extension

---
+++ TTD/ReplayApi/TtdDap/extension/sampleWorkspace/MyApp/MyApp.cpp
@@ -0,0 +1,39 @@
+﻿// MyApp.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World 1!\n";
+    std::cout << "Hello World 2!\n";
+    std::cout << "Hello World 3!\n";
+    std::cout << "Hello World 4!\n";
+    std::cout << "Hello World 5!\n";
+    std::cout << "Hello World 6!\n";
+    std::cout << "Hello World 7!\n";
+    std::cout << "Hello World 8!\n";
+    std::cout << "Hello World 9!\n";
+    std::cout << "Hello World 10!\n";
+    std::cout << "Hello World 11!\n";
+    std::cout << "Hello World 12!\n";
+    std::cout << "Hello World 13!\n";
+    std::cout << "Hello World 14!\n";
+    std::cout << "Hello World 15!\n";
+    std::cout << "Hello World 16!\n";
+    std::cout << "Hello World 17!\n";
+    std::cout << "Hello World 18!\n";
+    std::cout << "Hello World 19!\n";
+    std::cout << "Hello World 20!\n";
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
